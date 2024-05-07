ab8341e796818e0ee1b001944fb55d45183f1b64
Rodrigo Marí <rorro2001@gmail.com>
2024-05-06 21:37:32 -0300

    ejercicio actualizado

--- calculadora.js
+++ calculadora.js
@@ -1,12 +1,30 @@
-function sumar(a, b){
-    return a + b;
-}
+function pepito(cadena){
 
-function multiplicar(a, b){
-    return a * b;
+    if(cadena[0] == "/")
+    {
+        Delimitador = cadena[2]
+        cadena = cadena.slice(4)
+        SplitedCadena = cadena.split(Delimitador)
+    }
+    else
+        SplitedCadena = cadena.split(/,|\n/)
+
+    if(cadena[0] == "-")
+        return "No se permiten negativos"
+
+    let suma = 0
+    if(cadena == "")
+        return 0;
+
+    for( const s of SplitedCadena)
+    {
+        const Numero = Number(s)
+        suma = suma + Numero
+    }
+    return suma
+
 }
 
 module.exports = {
-    sumar,
-    multiplicar
+    pepito
 }
--- calculadora.test.js
+++ calculadora.test.js
@@ -1,26 +1,38 @@
 const assert = require('assert');
 const calculadora = require('./calculadora.js');
 
-it('Sumar 2 + 2', function () {
-    const a = 2;
-    const b = 2;
-    const result = 2 + 2;
-
-    assert.equal(calculadora.sumar(a, b), result);
-});
-
-it('Multiplicar 5 * 2', function () {
-    const a = 5;
-    const b = 2;
-    const result = 5 * 2;
-
-    assert.equal(calculadora.multiplicar(a, b), result);
-});
-
-it('Multiplicar por 0', function () {
-    const a = 0;
-    const b = 2;
+it('Vacio', function () {
+    const cadena = ""
     const result = 0;
 
-    assert.equal(calculadora.multiplicar(a, b), result);
+    assert.equal(calculadora.pepito(cadena), result);
 });
+
+it('Lleno', function () {
+    const cadena = "1,2"
+    const result = 3;
+
+    assert.equal(calculadora.pepito(cadena), result);
+});
+
+it('\n', function () {
+    const cadena = "1,2,4\n5,6"
+    const result = 18;
+
+    assert.equal(calculadora.pepito(cadena), result);
+});
+
+it('\n', function () {
+    const cadena = "//;\n1;2;4;5;6"
+    const result = 18;
+
+    assert.equal(calculadora.pepito(cadena), result);
+});
+
+it('\n', function () {
+    const cadena = "-5"
+    const result = "No se permiten negativos -5";
+
+    assert.equal(calculadora.pepito(cadena), result);
+});
+
